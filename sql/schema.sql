1322f4d784e3899b2c2f8546480af6bca3baecf2
gam0022 <gam0022@gmail.com>
2015-09-25 03:33:17 +0900

    INDEX外したのにスコア変わらない(15991)

--- sql/schema.sql
+++ sql/schema.sql
@@ -11,7 +11,5 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `user_id` int,
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
-  `succeeded` tinyint NOT NULL,
-  index(user_id, succeeded),
-  index(ip, succeeded)
+  `succeeded` tinyint NOT NULL
 ) DEFAULT CHARSET=utf8;
